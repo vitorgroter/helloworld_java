bbe493c66bbe20b14b0a5df684c3596af3f045cc
Felipe Dias <felipelimadiaz@yahoo.com.br>
2018-08-26 19:47:36 -0300

    insert error

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,12 @@
 import java.io.PrintStream;
 
-public class HelloWorld {
+public class HelloWorld
+{
     public static void main(String[] args) {
         print(System.out);
     }
 
-    public static void print(PrintStream out) {
+    public staic void print(PrintStream out) {
         out.println("Hello, World!");
     }
 }
